35f4eb0a76a0247f5263059c37e4896096051303
JSpiner <jspiner@naver.com>
2017-02-25 16:12:42 +0900

    add test application class

---
+++ app/src/main/java/net/jspiner/preferlib/TestApplication.java
@@ -0,0 +1,19 @@
+package net.jspiner.preferlib;
+
+import android.app.Application;
+
+/**
+ * Copyright 2017 JSpiner. All rights reserved.
+ *
+ * @author jspiner (jspiner@naver.com)
+ * @project PreferLib
+ * @since 17. 2. 25
+ */
+
+public class TestApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
