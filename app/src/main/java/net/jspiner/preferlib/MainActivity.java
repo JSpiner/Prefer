8d1c5537d9615d0aba7e48b9f6b889298f883cb7
JSpiner <jspiner@naver.com>
2017-02-25 16:04:42 +0900

    add module

--- app/src/main/java/net/jspiner/preferlib/MainActivity.java
+++ app/src/main/java/net/jspiner/preferlib/MainActivity.java
@@ -3,11 +3,16 @@ package net.jspiner.preferlib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.jspiner.prefer.Prefer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Prefer.init(getApplicationContext());
+        Prefer.set("Dd","dd");
     }
 }
